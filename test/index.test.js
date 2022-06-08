c5f443ddb32f08b862f59ea90db9dfc77c7a00bd
Ilya Afanasenko <fg1@mail.ru>
2022-06-08 21:57:14 +0400

    test: getLastBlock

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,25 @@
 const { expect } = require('chai');
 
-const { getBlockByHash } = require('#src');
+const { getBlockByHash, getLastBlock } = require('#src');
+
+const MILLISECONDS_IN_SECOND = 1000;
+const MILLISECONDS_IN_MINUTE = MILLISECONDS_IN_SECOND * 60;
 
 describe('getBlockByHash', () => {
-    it('should return the valid block', async function () {
-        this.timeout(10000);
-
+    it('should return the valid block', async () => {
         const exampleBlock = require('./data/valid-block');
         const block        = await getBlockByHash(exampleBlock.hash);
 
         expect(exampleBlock).deep.eq(block);
     });
+});
+
+describe('getLastBlock', () => {
+    it('should return a valid block', async () => {
+        const block       = await getLastBlock();
+        const blockTime   = block.time * MILLISECONDS_IN_SECOND;
+        const currentTime = Date.now();
+        
+        expect(currentTime - blockTime).below(30 * MILLISECONDS_IN_MINUTE);
+    })
 });
